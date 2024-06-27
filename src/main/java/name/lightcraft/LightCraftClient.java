52bb3fe4f8b6f780d8efa9986a9a18ebce3dbbf7
Demo_Bit@outlook.com <Demo_Bit@outlook.com>
2024-06-27 11:04:40 +0800

    Initial commit

---
+++ src/main/java/name/lightcraft/LightCraftClient.java
@@ -0,0 +1,10 @@
+package name.lightcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
